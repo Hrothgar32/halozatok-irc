890a46ac38eca4c5140187ddaf965dfe18a5c953
hrothgar32 <zold.almos@gmail.com>
2022-04-18 13:03:03 +0300

    Khmm

--- kliens.py
+++ kliens.py
@@ -9,15 +9,27 @@ client_socket.connect(("localhost", 8080))
 
 
 def reader():
+    global username
     while 1:
         message_str = client_socket.recv(2048).decode()
         message = message_str.split("#")
         if message[0] == "201":
             text = "{}:{}\n".format(message[1], message[2])
-            message_area["state"] = "normal"
-            message_area.insert("end", text)
-            message_area.see("end")
-            message_area["state"] = "disabled"
+        elif message[0] == "200":
+            text = "User {} has entered the chat!\n".format(message[1])
+        elif message[0] == "202":
+            text = "{}:{}\n".format(message[1], message[2])
+        elif message[0] == "203":
+            text = "\n".join(message[1:]) + "\n"
+        elif message[0] == "400":
+            text = "This username is already taken!\n"
+            username = None
+        elif message[0] == "401":
+            text = "No such user exists!\n"
+        message_area["state"] = "normal"
+        message_area.insert("end", text)
+        message_area.see("end")
+        message_area["state"] = "disabled"
 
 
 def callback(event):
@@ -27,10 +39,29 @@ def callback(event):
     if username:
         display_text = "{}:{}\n".format(username, text)
         message_area["state"] = "normal"
-        message_area.insert("end", display_text)
-        message_area.see("end")
-        message_area["state"] = "disabled"
-        client_socket.sendall("{}#GLOBAL#{}".format(username, text).encode())
+        message = text.split(" ")
+        if message[0] == "/p":
+            client_socket.sendall(
+                "{}#PRIVATE#{}#{}".format(
+                    username, message[1], " ".join(message[2:])
+                ).encode()
+            )
+            message_area.insert(
+                "end", "{}:".format(username) + " ".join(message[2:]) + "\n"
+            )
+            message_area.see("end")
+            message_area["state"] = "disabled"
+        elif message[0] == "/exit":
+            client_socket.sendall("{}#EXIT".format(username).encode())
+        elif message[0] == "/list":
+            client_socket.sendall("{}#LIST".format(username).encode())
+        else:
+            client_socket.sendall(
+                "{}#GLOBAL#{}".format(username, " ".join(message)).encode()
+            )
+            message_area.insert("end", display_text)
+            message_area.see("end")
+            message_area["state"] = "disabled"
     else:
         stuff = text.split(" ")
         if stuff[0] != "/login":
@@ -52,6 +83,6 @@ input_entry = Entry(m)
 input_entry.grid(row=1, column=1)
 input_entry.bind("<Return>", callback)
 
-threading.Thread(target=reader).start()
+threading.Thread(target=reader, daemon=True).start()
 
 m.mainloop()
--- server.py
+++ server.py
@@ -9,17 +9,36 @@ clients = {}
 
 
 def serve_client(connection, address):
+    thread_key = None
     while 1:
         message_str = connection.recv(2048).decode()
         message = message_str.split("#")
         if message[1] == "LOGIN":
+            if message[0] in clients:
+                connection.sendall("400".encode())
             clients[message[0]] = connection
-            connection.sendall("200#OK".encode())
+            thread_key = message[0]
+            new_message = "200#{}".format(message[0]).encode()
+            for key in clients.keys():
+                clients[key].sendall(new_message)
         elif message[1] == "GLOBAL":
             new_message = "201#{}#{}".format(message[0], message[2]).encode()
             for key in clients.keys():
                 if key != message[0]:
                     clients[key].sendall(new_message)
+        elif message[1] == "PRIVATE":
+            new_message = "202#{}#{}".format(message[0], message[3]).encode()
+            if message[2] in clients:
+                clients[message[2]].sendall(new_message)
+            else:
+                clients[message[0]].sendall("401".encode())
+        elif message[1] == "EXIT":
+            connection.close()
+            del clients[thread_key]
+            break
+        elif message[1] == "LIST":
+            new_message = "203#{}".format("#".join(clients.keys())).encode()
+            clients[message[0]].sendall(new_message)
 
 
 while 1:
